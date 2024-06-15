625c74532ab9af68a3f02826c74fcdcc868216c7
剑倾 <1904741371@qq.com>
2024-06-15 18:27:01 +0800

    🐳 chore:更换打包工具-rollup，添加es5打包结果

---
+++ rollup.config.es5.mjs
@@ -0,0 +1,28 @@
+import { defineConfig } from 'rollup'
+import typescript from '@rollup/plugin-typescript'
+import { babel } from '@rollup/plugin-babel'
+
+export default defineConfig({
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/es5/index.cjs.js',
+      format: 'cjs',
+    },
+    {
+      file: 'dist/es5/index.esm.js',
+      format: 'esm',
+    },
+    {
+      file: 'dist/es5/index.umd.js',
+      format: 'umd',
+      name: 'AsyncEventChannel',
+    },
+  ],
+  plugins: [
+    typescript(),
+    babel({
+      extensions: ['.ts'],
+    }),
+  ],
+})
---
+++ rollup.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from 'rollup'
+import typescript from '@rollup/plugin-typescript'
+
+export default defineConfig({
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+    },
+    {
+      file: 'dist/index.umd.js',
+      format: 'umd',
+      name: 'AsyncEventChannel',
+    },
+  ],
+  plugins: [
+    typescript(),
+  ],
+})
