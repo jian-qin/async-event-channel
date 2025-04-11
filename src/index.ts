fb1d199ba58f4158d8908091b741915ea2008184
剑倾 <1904741371@qq.com>
2025-04-11 17:39:22 +0800

    ✨ feat: hook方法、size方法

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 type Listener = (params: unknown, result: Result) => unknown
-type Result = Readonly<{
+type Target = string | RegExp
+type HookType = 'on' | 'emit' | 'off' | 'reply'
+
+export type Result = Readonly<{
   id: number
   has: () => boolean
   off: () => void
 }>
-type ListenersValue = {
+
+export type ListenersValue = {
   event: string
   listener: Listener
   result: Result
@@ -13,7 +17,8 @@ type ListenersValue = {
     once?: boolean
   }
 }
-type TriggersValue = {
+
+export type TriggersValue = {
   event: string
   params: unknown
   result: Result
@@ -25,14 +30,32 @@ type TriggersValue = {
   replys: Map<number, unknown>
 }
 
+type HooksValue = {
+  target: Target | number
+  has: (id: number) => boolean
+  listener: (params: ListenersValue | TriggersValue, result: Result) => void
+  result: Result
+  options?: {
+    type?: HookType | 'all'
+    once?: boolean
+  }
+}
+
 export default class AsyncEventChannel {
   private _id = 0
   private _listeners = new Map<number, ListenersValue>()
   private _triggers = new Map<number, TriggersValue>()
+  private _hooks = new Map<number, HooksValue>()
 
-  private _map_get<T extends ListenersValue | TriggersValue>(map: Map<number, T>, target: string | RegExp) {
+  private _map_get<T extends ListenersValue | TriggersValue>(map: Map<number, T>, target: Target) {
     const has = typeof target === 'string' ? (event: string) => event === target : (event: string) => target.test(event)
     return [...map.values()].filter(({ event }) => has(event))
+  }
+
+  private _map_del(map: Map<number, ListenersValue | TriggersValue>, id: number) {
+    if (!map.has(id)) return
+    this._hook_run(id, 'off')
+    map.delete(id)
   }
 
   private _listeners_add(value: Pick<ListenersValue, 'event' | 'listener' | 'options'>) {
@@ -40,9 +63,10 @@ export default class AsyncEventChannel {
     const result = Object.freeze({
       id,
       has: () => this._listeners.has(id),
-      off: () => this._listeners_del(id),
+      off: () => this._map_del(this._listeners, id),
     })
     this._listeners.set(id, { ...value, result })
+    this._hook_run(id, 'on')
     return result
   }
 
@@ -51,22 +75,11 @@ export default class AsyncEventChannel {
     const result = Object.freeze({
       id,
       has: () => this._triggers.has(id),
-      off: () => this._triggers_del(id),
+      off: () => this._map_del(this._triggers, id),
     })
     this._triggers.set(id, { ...value, result, replys: new Map() })
+    this._hook_run(id, 'emit')
     return result
-  }
-
-  private _listeners_del(id: number) {
-    const item = this._listeners.get(id)
-    if (!item) return
-    this._listeners.delete(id)
-  }
-
-  private _triggers_del(id: number) {
-    const item = this._triggers.get(id)
-    if (!item) return
-    this._triggers.delete(id)
   }
 
   private async _run(listener: ListenersValue, trigger: TriggersValue) {
@@ -77,6 +90,7 @@ export default class AsyncEventChannel {
       _result = await Promise.resolve(_result)
     }
     trigger.replys.set(listener.result.id, _result)
+    this._hook_run(trigger.result.id, 'reply')
     onReply(trigger.replys, trigger.result)
     if (listener.options?.once) {
       listener.result.off()
@@ -100,6 +114,34 @@ export default class AsyncEventChannel {
     triggers.forEach((trigger) => this._run(listener, trigger))
   }
 
+  private _hook_has(target: HooksValue['target']) {
+    let _has: (value: ListenersValue | TriggersValue) => boolean
+    if (typeof target === 'number') {
+      _has = ({ result }) => result.id === target
+    } else if (typeof target === 'string') {
+      _has = ({ event }) => event === target
+    } else {
+      _has = ({ event }) => target.test(event)
+    }
+    return (id: number) => {
+      const value = this._listeners.get(id) || this._triggers.get(id)
+      return value ? _has(value) : false
+    }
+  }
+
+  private _hook_run(id: number, type: HookType) {
+    const params = (this._listeners.get(id) || this._triggers.get(id))!
+    this._hooks.forEach(({ has, listener, options, result }) => {
+      const _type = options?.type || 'all'
+      if (_type !== type && _type !== 'all') return
+      if (!has(id)) return
+      listener({ ...params }, result)
+      if (options?.once) {
+        result.off()
+      }
+    })
+  }
+
   on(event: string, listener: Listener, options?: ListenersValue['options']) {
     const result = this._listeners_add({ event, listener, options })
     this._emit_run(result.id)
@@ -110,12 +152,12 @@ export default class AsyncEventChannel {
     const result = this._triggers_add({ event, params, options })
     this._on_run(result.id)
     if (!options?.wait) {
-      this._triggers_del(result.id)
+      this._map_del(this._triggers, result.id)
     }
     return result
   }
 
-  off(target: string | RegExp, type: 'all' | 'on' | 'emit' = 'all') {
+  off(target: Target, type: 'all' | 'on' | 'emit' = 'all') {
     if (type === 'on' || type === 'all') {
       this._map_get(this._listeners, target).forEach(({ result }) => result.off())
     }
@@ -124,5 +166,23 @@ export default class AsyncEventChannel {
     }
   }
 
-  has(target: string | RegExp) {}
+  hook(target: HooksValue['target'], listener: HooksValue['listener'], options?: HooksValue['options']) {
+    const id = ++this._id
+    const result = Object.freeze({
+      id,
+      has: () => this._hooks.has(id),
+      off: () => {
+        this._hooks.delete(id)
+      },
+    })
+    this._hooks.set(id, { target, listener, options, result, has: this._hook_has(target) })
+    return result
+  }
+
+  size(target: Target) {
+    return {
+      on: this._map_get(this._listeners, target).length,
+      emit: this._map_get(this._triggers, target).length,
+    }
+  }
 }
